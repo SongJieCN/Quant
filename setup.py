5432a24d181e60ff6a3c5de01fc7942d62e89259
SONG.JIE <songjie1984@gmail.com>
2017-03-08 14:13:33 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Quant',
+    version='0.1',
+    packages=[''],
+    url='www.tigerquant.com',
+    license='MIT',
+    author='SongJie',
+    author_email='songjie1984@gmail.com',
+    description=''
+)
